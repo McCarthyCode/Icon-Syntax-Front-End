69f5e5551d50911dd6a82e278c94e0ebbdeda6aa
Matt McCarthy <matt@mccarthycode.com>
2021-08-04 11:13:56 -0500

    Add infinite scrolling to Find page
    - Split loading attribute into loadingCategories and loadingIcons so
      tile and categories can be visible while icons are loading
    - Change *ngIf directives in template to style bindings so the page
      doesn't scroll to the top on infinite scroll events
    - Add spinner for loading icons that doesn't appear when infinite scroll
      spinner is visible
    - Update IconService to accept page as a parameter and removed unused
      list$ attribute

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -1,11 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { IconsService } from '../icons.service';
 import { Category } from '../models/category.model';
 import { Icon } from '../models/icon.model';
+
+const emptyCategories: Category.IClientDataList = {
+  results: [],
+  retrieved: new Date(),
+};
+const emptyIcons: Icon.IClientDataList = {
+  results: [],
+  pagination: {
+    totalResults: 0,
+    maxResultsPerPage: 100,
+    numResultsThisPage: 0,
+    thisPageNumber: 0,
+    totalPages: 0,
+    prevPageExists: false,
+    nextPageExists: false,
+  },
+  retrieved: new Date(),
+};
 
 @Component({
   selector: 'app-categories',
@@ -15,58 +33,72 @@ import { Icon } from '../models/icon.model';
 export class CategoriesPage implements OnInit {
   // Behavior Subjects
   category$ = new BehaviorSubject<Category.IClientData>(null);
-  categories$ = new BehaviorSubject<Category.IClientDataList>(null);
-  icons$ = new BehaviorSubject<Icon.IClientDataList>({
-    results: [],
-    pagination: {
-      totalResults: 0,
-      maxResultsPerPage: 100,
-      numResultsThisPage: 0,
-      thisPageNumber: 1,
-      totalPages: 0,
-      prevPageExists: false,
-      nextPageExists: false,
-    },
-    retrieved: new Date(),
-  });
+  categories$ = new BehaviorSubject<Category.IClientDataList>(emptyCategories);
+  icons$ = new BehaviorSubject<Icon.IClientDataList>(emptyIcons);
 
   // Subscriptions
   iconsSub: Subscription;
   categoriesSub: Subscription;
 
-  // Misc.
-  loading = true;
+  // Spinners
+  loadingCategories = true;
+  loadingIcons = true;
+
+  // Navigation history
   breadcrumbs: Category.IClientData[] = [];
+
+  // Back-end parameters
   query = '';
+  page = 1;
 
   constructor(private _http: HttpClient, private _iconsSrv: IconsService) {}
 
   ngOnInit() {
     this.categoriesSub = this.listCategories().subscribe(() => {
+      this.loadingCategories = false;
       this.iconsSub = this.listIcons().subscribe((icons) => {
         this.icons$.next(icons);
-        this.loading = false;
+        this.loadingIcons = false;
       });
     });
   }
 
+  resetCategories() {
+    this.loadingCategories = true;
+  }
+
+  resetIcons(empty = true) {
+    this.loadingIcons = true;
+
+    if (empty) {
+      this.page = 1;
+      this.icons$.next(emptyIcons);
+    } else {
+
+    this.page++;
+    }
+  }
+
   searchbar(query: string): void {
-    this.loading = true;
+    this.resetCategories();
+    this.resetIcons();
+
     this.query = query;
+
     this.categoriesSub = this.category$.subscribe(() => {
+      this.loadingCategories = false;
       this.iconsSub = this.listIcons().subscribe((icons) => {
         this.icons$.next(icons);
-        this.loading = false;
+        this.loadingIcons = false;
       });
     });
   }
 
   retrieveCategory(id: number): Observable<Category.IClientData> {
-    this.loading = true;
-
     return this._http
       .get<Category.IResponseBody>(environment.apiBase + '/categories/' + id)
       .pipe(
+        debounceTime(250),
         map((body) => {
           const retrieved = new Date();
           const category: Category.IClientData = {
@@ -87,10 +119,10 @@ export class CategoriesPage implements OnInit {
   }
 
   listCategories(): Observable<Category.IClientDataList> {
-    this.loading = true;
     return this._http
       .get<Category.IResponseBodyList>(environment.apiBase + '/categories')
       .pipe(
+        debounceTime(250),
         map((body) => {
           const clientDataList: Category.IClientDataList = {
             ...body,
@@ -108,39 +140,67 @@ export class CategoriesPage implements OnInit {
   }
 
   listIcons(): Observable<Icon.IClientDataList> {
-    this.loading = true;
     const category = this.category$.value;
 
     return this._iconsSrv.list(
       this.query ? this.query : undefined,
-      category ? category.id : undefined
+      category ? category.id : undefined,
+      this.page
     );
   }
 
   clickCategory(id: number): void {
+    this.resetCategories();
+    this.resetIcons();
+
     this.breadcrumbs.push(this.category$.value);
 
     this.retrieveCategory(id).subscribe(() => {
-      this.categories$.subscribe(() =>
+      this.categories$.subscribe(() => {
+        this.loadingCategories = false;
         this.listIcons().subscribe((icons) => {
           this.icons$.next(icons);
-          this.loading = false;
-        })
-      );
+          this.loadingIcons = false;
+        });
+      });
     });
   }
 
   clickBack(): void {
-    const category = this.breadcrumbs.pop();
+    this.resetCategories();
+    this.resetIcons();
 
+    const category = this.breadcrumbs.pop();
     if (category === null) {
-      this.listCategories().subscribe(() =>
-        this.listIcons().subscribe(() => (this.loading = false))
-      );
+      this.listCategories().subscribe(() => {
+        this.loadingCategories = false;
+        this.listIcons().subscribe(() => (this.loadingIcons = false));
+      });
     } else {
       this.retrieveCategory(category.id).subscribe(
-        () => (this.loading = false)
+        () => (this.loadingCategories = false)
       );
     }
+  }
+
+  nextPage($event): void {
+    this.resetIcons(false);
+
+    this.categoriesSub = this.categories$.subscribe(() => {
+      const page = this.icons$.value.pagination.thisPageNumber;
+      this.iconsSub = this.listIcons().subscribe((icons) => {
+        const updated = this.icons$.value;
+
+        if (updated) {
+          updated.results.push(...icons.results);
+          this.icons$.next(updated);
+        } else {
+          this.icons$.next(icons);
+        }
+
+        $event.target.complete();
+        this.loadingIcons = false;
+      });
+    });
   }
 }
--- src/app/icons.service.ts
+++ src/app/icons.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Icon } from './models/icon.model';
-import { IPagination } from './models/pagination.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IconsService {
-  list$ = new BehaviorSubject<Icon.IClientDataList>(null);
-
   constructor(private _http: HttpClient) {}
 
-  list(query?: string, categoryId?: number): Observable<Icon.IClientDataList> {
+  list(
+    query?: string,
+    categoryId?: number,
+    page: number = 1
+  ): Observable<Icon.IClientDataList> {
     let params = {};
     if (categoryId) {
       params = { category: categoryId };
@@ -22,18 +23,22 @@ export class IconsService {
     if (query) {
       params = { ...params, search: query };
     }
+    if (page) {
+      params = { ...params, page: page };
+    }
+
 
     return this._http
       .get<Icon.IResponseBodyList>(environment.apiBase + '/icons', {
         params: params,
       })
       .pipe(
+        debounceTime(250),
         map((body) => {
           const clientData = {
             ...body,
             retrieved: new Date(),
           };
-          this.list$.next(clientData);
 
           return clientData;
         })
