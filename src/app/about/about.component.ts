5652469ec53d9deedb066ca133b1730fe80d58e4
Matt McCarthy <matt@mccarthycode.com>
2021-08-24 23:12:12 -0500

    Add slideshow to About page with navigation column
    Most slides are done, except for the last two. A .no-select class has
    been added to AppComponent.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+interface IContent {
+  header: string;
+  page: number;
+}
 
 @Component({
   selector: 'app-about',
@@ -6,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  page = 1;
+  contents: IContent[] = [
+    { header: 'Title', page: 1 },
+    { header: 'About Our Vision', page: 2 },
+    { header: 'Executive Summary', page: 3 },
+    { header: 'About Syntax', page: 4 },
+    { header: 'About Icons', page: 5 },
+    { header: 'About Parts of Speech', page: 6 },
+    { header: 'About Words', page: 7 },
+    { header: 'About Mary Beth', page: 8 },
+    { header: 'About Matt & Robert', page: 9 },
+    { header: 'History of ILS', page: 10 },
+    { header: 'About Demand', page: 11 },
+    { header: 'About Us', page: 12 },
+    { header: 'Contact Us', page: 13 },
+    { header: 'Community Notes', page: 14 },
+  ];
 
-  constructor() { }
+  @ViewChild('slides', { static: true }) slides: IonSlides;
+
+  constructor() {}
 
   ngOnInit() {}
 
+  updatePage() {
+    this.slides.getActiveIndex().then((index) => {
+      this.page = index + 1;
+    });
+  }
+
+  onIonSlideNextEnd() {
+    this.updatePage();
+  }
+
+  onIonSlidePrevEnd() {
+    this.updatePage();
+  }
+
+  onNavigate(page: number) {
+    this.slides.slideTo(page - 1);
+  }
+
+  onMouseEnter($event: Event) {
+    ($event.target as HTMLIonTextElement).color = 'warning';
+  }
+
+  onMouseLeave($event: Event, page: number) {
+    this.slides.getActiveIndex().then((index) => {
+      ($event.target as HTMLIonTextElement).color =
+        page === index + 1 ? 'warning' : 'medium';
+    });
+  }
 }
