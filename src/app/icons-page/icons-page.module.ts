4f62681a79fc9a78cc764b18c5ed346082a71b3c
Matt McCarthy <matt@mccarthycode.com>
2021-12-03 20:29:45 -0600

    Move IconDetail template tags to AppComponent
    This fixes the issue on IconsPage where the panel was rendering behind
    toolbars.

--- src/app/icons-page/icons-page.module.ts
+++ src/app/icons-page/icons-page.module.ts
@@ -6,13 +6,13 @@ import { IonicModule } from '@ionic/angular';
 
 import { IconsPageRoutingModule } from './icons-page-routing.module';
 
+import { ComponentsModule } from './components.module';
+
 import {
   BrowseComponent,
   CategoryNodeComponent,
 } from './browse/browse.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
-import { IconDetailComponent } from '../icon-detail/icon-detail.component';
-import { ComponentsModule } from './components.module';
 
 @NgModule({
   imports: [
