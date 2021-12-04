7c969cd75e131525fa3d7b0b21d8ee16f847db20
Matt McCarthy <matt@mccarthycode.com>
2021-12-03 20:27:42 -0600

    Optimize and organize IconsPage components module

--- src/app/icons-page/components.module.ts
+++ src/app/icons-page/components.module.ts
@@ -2,14 +2,16 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+
 import { IconComponent } from '../icon/icon.component';
 import { IconDetailComponent } from '../icon-detail/icon-detail.component';
 
+const components = [IconComponent, IconDetailComponent]
 
 @NgModule({
-    declarations: [IconComponent, IconDetailComponent], // Import all component here
-    imports: [CommonModule, FormsModule, IonicModule],
-    exports: [IconComponent, IconDetailComponent], // Import all component here
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  declarations: components,
+  imports: [CommonModule, FormsModule, IonicModule],
+  exports: components,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
