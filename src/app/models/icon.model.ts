f30974fa205cf961d82b06e03e30cf97661e4a1e
Matt McCarthy <matt@mccarthycode.com>
2022-01-18 14:03:01 -0600

    [WIP] Edit GenericService and apply to IconService
    - Define generic ResponseBody to IClientData and ResponseBodyList to
      IClientDataList conversion methods instead of forcing them to be
      defined in child constructors
    - Make IconsService Generic
    - Edit interfaces in Model namespace
      - Remove IParams as id should be defined in child interfaces in case
        it is a data type other than number
      - Define generic response interfaces with success/error strings
    - Update Icon model namespace to inherit from its parent, Model
    - Define Data and Datum in children of Model

--- src/app/models/icon.model.ts
+++ src/app/models/icon.model.ts
@@ -9,18 +9,23 @@ export namespace Icon {
     icon: string;
     md5: string;
   }
+
+  interface IDatum extends Model.IDatum<IIcon> {}
+  interface IData extends Model.IData<IIcon> {}
+
+  export interface IRequestBody extends Model.IRequestBody {}
   export interface IResponseBody
     extends Model.IResponseBody,
-      IIcon,
+      IDatum,
       Partial<Model.ISuccessResponse>,
       Partial<Model.IErrorResponse> {}
   export interface IResponseBodyList
     extends Model.IResponseBodyList,
+      IData,
       Partial<Model.ISuccessResponse>,
       Partial<Model.IErrorResponse> {}
   export interface IClientData extends Model.IClientData {}
   export interface IClientDataList extends Model.IClientDataList {}
-  export interface IRequestBody extends Model.IRequestBody {}
 
   export const emptyRequestBody: IRequestBody = {
     icon: null,
--- src/app/models/model.ts
+++ src/app/models/model.ts
@@ -1,10 +1,10 @@
 import { IPagination } from '../interfaces/pagination.interface';
 
 export namespace Model {
-  interface IDatum<IModel> {
+  export interface IDatum<IModel> {
     data: IModel;
   }
-  interface IData<IModel> {
+  export interface IData<IModel> {
     data: IModel[];
     pagination: IPagination;
   }
@@ -14,8 +14,8 @@ export namespace Model {
 
   export interface IModel {}
   export interface IRequestBody extends Partial<IModel> {}
-  export interface IResponseBody extends IDatum<IModel> {}
-  export interface IResponseBodyList extends IData<IModel> {}
+  export interface IResponseBody {}
+  export interface IResponseBodyList {}
   export interface IClientData extends IDatum<IModel>, IClientDataCommon {}
   export interface IClientDataList extends IData<IModel>, IClientDataCommon {}
 
